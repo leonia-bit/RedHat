c67abd5ca0414524525b91eed44214b9c51600c4
mkbc3 <leonia.klebanov@gmail.com>
2019-12-23 21:10:16 +0200

    Init RedHat project environment.

---
+++ FindRegExInFile/__init__.py
@@ -0,0 +1,19 @@
+x = 5
+y = 7
+print(x + y)
+if (y > x):
+    print(str(y) + " is greather then: " + str(x))
+
+num = 0
+if (num>0):
+    print(str(num) + " this is positive number")
+elif (num == 0):
+    print("Num is:" + str(num))
+else:
+    print(str(num) + " this is negative number")
+
+#list
+elements = [1,2,3,4,5]
+for val in elements:
+    print(val)
+
