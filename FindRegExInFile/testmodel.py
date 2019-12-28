2198d6f09a1ca2cec7c2d44e61bc47862c54ceba
mkbc3 <leonia.klebanov@gmail.com>
2019-12-28 08:32:02 +0200

    1. unittest: for controller unit added

---
+++ FindRegExInFile/testmodel.py
@@ -0,0 +1,26 @@
+from cmdline import GetUserOptions
+from model import FilesData
+import unittest
+
+
+# import HTMLTestRunner
+
+class TestCommandLine(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls) -> None:
+        cls.cmd_line = GetUserOptions()
+        cls.cmd_line.set_user_options()
+
+    def test_cmdline(self):
+        f_list, regexp, o_format = self.cmd_line.get_user_options(True)
+        file_data_list = FilesData(f_list).get_all()
+        assert file_data_list != [], "Check why: files data not available."
+
+    @classmethod
+    def tearDownClass(cls):
+        print("Test [model] Completed Successfully!")
+
+
+if __name__ == '__main__':
+    unittest.main()
+
